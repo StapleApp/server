56c97822071fd818dfad60d0240291a78170a261
YusufBalmumcu <yusufbalmumcu1@hotmail.com>
2025-03-29 14:58:23 +0300

    Enhance user connection handling and update active users list in Socket.IO

--- src/server.js
+++ src/server.js
@@ -1,25 +1,31 @@
-// server.js (Node.js ve Express ile)
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Bağlantı sağlandığında kullanıcıya "Client bağlandı" mesajı gönder
-io.on('connection', (socket) => {
-  console.log('A user connected');
-  
-  // Bağlanan client'a mesaj gönder
-  socket.emit('message', 'Client bağlandı');
+// Aktif kullanıcılar listesi
+let activeUsers = [];
 
-  // Client bağlantıdan çıktığında mesaj ver
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
+io.on("connection", (socket) => {
+  console.log("New user connected:", socket.id);
+
+  // Kullanıcı bağlantı sağladığında aktif kullanıcılar listesine ekle
+  activeUsers.push(socket.id);
+
+  // Aktif kullanıcıları tüm clientlara gönder
+  io.emit("update-users", activeUsers);
+
+  // Kullanıcı bağlantıyı kestiğinde listeden çıkar
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+    activeUsers = activeUsers.filter((userId) => userId !== socket.id);
+    io.emit("update-users", activeUsers);
   });
 });
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
+server.listen(3001, () => {
+  console.log("Server is running on port 3001");
 });

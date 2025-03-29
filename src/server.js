a6076fb80a569892ecd785015c2efd5eeb468043
YusufBalmumcu <yusufbalmumcu1@hotmail.com>
2025-03-29 10:54:59 +0300

    Add .gitignore and set up basic Express server with Socket.IO for demo purpose

--- src/server.js
+++ src/server.js
@@ -1,60 +1,25 @@
-import { io } from 'socket.io-client';
+// server.js (Node.js ve Express ile)
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
-const socket = io('http://localhost:3000'); // Sunucuya 3000 portundan bağlan
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
-// Kullanıcı adını sunucuya göndermek için 'set-username' olayını tetikler
-export const setUsername = (username) => {
-  socket.emit('set-username', username);
-};
+// Bağlantı sağlandığında kullanıcıya "Client bağlandı" mesajı gönder
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  
+  // Bağlanan client'a mesaj gönder
+  socket.emit('message', 'Client bağlandı');
 
-// Kullanıcının bir odaya katılması için 'join-room' olayını tetikler
-export const joinRoom = () => {
-  socket.emit('join-room');
-};
+  // Client bağlantıdan çıktığında mesaj ver
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
 
-// Yeni bir kullanıcı bağlandığında 'user-connected' olayını dinler
-export const onUserConnected = (callback) => {
-  socket.on('user-connected', callback);
-};
-
-// Bir kullanıcı ayrıldığında 'user-disconnected' olayını dinler
-export const onUserDisconnected = (callback) => {
-  socket.on('user-disconnected', callback);
-};
-
-// Kullanıcı listesini güncellemek için 'update-users' olayını dinler
-export const onUpdateUsers = (callback) => {
-  socket.on('update-users', callback);
-};
-
-// Başka bir kullanıcıdan gelen video görüşmesi davetini dinlemek için 'offer' olayını dinler
-export const onOffer = (callback) => {
-  socket.on('offer', callback);
-};
-
-// Kullanıcıdan gelen video çağrısını yanıtlamak için 'answer' olayını dinler
-export const onAnswer = (callback) => {
-  socket.on('answer', callback);
-};
-
-// ICE adaylarını almak için 'candidate' olayını dinler
-export const onCandidate = (callback) => {
-  socket.on('candidate', callback);
-};
-
-// Sunucuya bir video görüşmesi teklifi göndermek için 'offer' olayını tetikler
-export const sendOffer = (userId, offer) => {
-  socket.emit('offer', { userId, offer });
-};
-
-// Sunucuya bir video görüşmesi yanıtı göndermek için 'answer' olayını tetikler
-export const sendAnswer = (userId, answer) => {
-  socket.emit('answer', { userId, answer });
-};
-
-// Sunucuya ICE adayı göndermek için 'candidate' olayını tetikler
-export const sendCandidate = (userId, candidate) => {
-  socket.emit('candidate', { userId, candidate });
-};
-
-export default socket;
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});

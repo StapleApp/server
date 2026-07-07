54c6cb120860e436c32bf3befa733996662653e7
YusufBalmumcu <yusufbalmumcu1@hotmail.com>
2026-07-08 01:24:22 +0300

    refactoring for old files

--- test/test.js
+++
@@ -1,7 +0,0 @@
-import { getGroupById, createGroup } from '../src/services/groupService.js';
-
-
-// const a = await createGroup("Test group", ["user1", "user2", "user3"]);
-const b = await getGroupById('ojN1U6abfFpp83DEoBoc');
-
-console.log(b);
